f5389c6853411284baa92f25a45f83710bd63d8f
Joshua Rubin <jrubin@zvelo.com>
2014-01-09 00:42:23 -0700

    add newrelic
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

---
+++ newrelic.js
@@ -0,0 +1,10 @@
+/**
+ * New Relic agent configuration.
+ *
+ * DO NOT EDIT
+ *
+ * Use environment variables as described:
+ * https://docs.newrelic.com/docs/nodejs/configuring-nodejs-with-environment-variables
+ */
+
+exports.config = {};
